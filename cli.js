1e85c82aa8c9906d908f4a329f99c9fe02a6ae29
amit-upadhyay-it <youarefreak1@gmail.com>
2017-09-23 16:35:31 +0530

    some issue persists

--- cli.js
+++ cli.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 'use strict';
 var meow = require('meow');
-var checkpackage = require('./');
+var checkpackage = require('./lib/index.js');
 
 var cli = meow({
     help: [
         'Usage',
-        '  checkpackage <input>',
+        '  checkpackage <module name>',
         '',
         'Example',
-        '  checkpackage unicorn'
+        '  checkpackage unicorn',
+        '',
+        'Developer',
+        '  Amit Upadhyay (github.com/amit-upadhyay-it)'
     ].join('\n')
 });
 
+console.log('Searching ...');
 checkpackage(cli.input[0]);
